f236294a2daa17b2cab10e1d733628bae5e87818
snbreem <snbreem@yahoo.com>
2020-02-24 10:35:48 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
 Route::get('profile',function(){
    return 'profile';
 });
